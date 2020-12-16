26610009b8e9e0261bf91b2703c9224db0bf5336
unknown <hconway@luc.edu>
2020-12-16 14:49:21 -0600

    part 1 complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Fotos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://allpetnews.com/wp-content/uploads/2013/04/Trending-Dog-Tights-3-628.jpg';
+  image3 = 'https://content.fun-japan.jp/renewal-prod/cms/articles/content/50feb476405c5bfa170649cf0f661071d1b7891c.jpg';
 
   constructor() { }
 
